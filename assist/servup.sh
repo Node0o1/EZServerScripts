90471bd281bef7abaf1725230a99dd654d208508
Node001 <node0o1@proton.me>
2024-07-15 19:44:37 -0400

    Update servup.sh

--- assist/servup.sh
+++ assist/servup.sh
@@ -14,17 +14,20 @@ echo "openning port\s.........";
 echo "attempt port# 80:http";
 echo "please wait";
 ufw allow http;
-echo "port 80:HTTP openned";
-echo "openning port\s.........";
+echo "port 80:HTTP openned\n";
 echo "attempt port# 443:https";
 echo "please wait";
 ufw allow https;
-echo "port 443:HTTPS openned";
+echo "port 443:HTTPS openned\n";
+echo "attempt port# 22:SSH";
+echo "please wait";
+ufw allow ssh;
+echo "port 22:SSH openned\n";
 
-/home/developer/assist/servstat.sh;
+./servstat.sh;
 
 echo "                  THE APACHE2 AND MYSQL SERVICES HAVE BEEN STARTED";
-echo "                        PORT# 80:HTTP / 443:HTTPS HAS BEEN SET TO ALLOW";
+echo "                        PORT# 80:HTTP / 443:HTTPS / 22:SSH HAVE BEEN SET TO ALLOW";
 echo "";
 echo "                       L.A.M.P.stack Has Been Activated !::!";
 echo "";
